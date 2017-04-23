9f84369485e2ea972a611b06d3c80685aa2d88d3
Hans Kindberg <hans.kindberg@gmail.com>
2017-04-23 21:48:14 +0200

    VS-2017

---
+++ NET-Standard/Project/Foo.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace HansKindberg
+{
+	public class Foo : IDisposable
+	{
+		#region Properties
+
+		protected internal virtual string InternalValue => "Test";
+		public virtual string Value => "Test";
+
+		#endregion
+
+		#region Methods
+
+		public void Dispose()
+		{
+			this.Dispose(true);
+
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing) {}
+
+		#endregion
+	}
+}
